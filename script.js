67b47091ffc1edb64480965415aa4052b7570bbf
LuukvandenBerg <luuk.berg1999@gmail.com>
2016-02-18 11:23:03 +0100

    Tab

---
+++ script.js
@@ -0,0 +1,24 @@
+$('.knoppen').each( function() {
+
+	var dezeKnop = $(this);
+	var actieveKnop = dezeKnop.find('li.actief');
+	var link = actieveKnop.find('a');
+	var actiefPaneel = $(link.attr('href'));
+
+	dezeKnop.on('click', 'a', function(e){
+		e.preventDefault();
+		var link = $(this);
+		var beoogdId = this.hash;
+
+		if (beoogdId && !link.parent().is('.actief')){
+			actiefPaneel.removeClass('actief');
+			actieveKnop.removeClass('actief');
+
+			actiefPaneel = $(beoogdId).addClass('actief');
+			actieveKnop = link.parent().addClass('actief');
+		}
+	})
+
+});
+
+$('.paneel').css('display', 'none')
